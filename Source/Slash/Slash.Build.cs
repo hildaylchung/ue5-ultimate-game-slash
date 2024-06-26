cbbc282569f24d748b18571b05b8d06de13c4827
hilda <hildachung0602@gmail.com>
2024-06-26 15:28:05 +0100

    fix: error crashing ue because gameabilities not found

--- Source/Slash/Slash.Build.cs
+++ Source/Slash/Slash.Build.cs
@@ -11,11 +11,7 @@ public class Slash : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ActionRPG", "Niagara", "HairStrandsCore"  });
 
-		PrivateDependencyModuleNames.AddRange(
-			new string[] {
-				"GameplayAbilities"
-			}
-		);
+		PrivateDependencyModuleNames.AddRange(new string[] {});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
